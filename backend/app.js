cd928e844960b4b019c157504e0cedce4208973e
Bruno Martins <b.gabriel.ma@gmail.com>
2019-11-06 16:07:06 +0000

    Update structure

--- backend/app.js
+++ backend/app.js
@@ -1,39 +1,41 @@
-const createError = require('http-errors')
-const express = require('express')
-const path = require('path')
-const cookieParser = require('cookie-parser')
-const logger = require('morgan')
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const indexRouter = require('./routes/index')
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const app = express()
-
-// custom middlewares
-const db = require('./config/db')
-const errorHandler = require('./middlewares/errorHandler')
-
-// put db object into app variable
-app.db = db
+var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-/* Routes zone */
-app.use('/', indexRouter)
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404))
-})
+  next(createError(404));
+});
 
 // error handler
-app.use(errorHandler)
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-module.exports = app
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
--- backend/config/db.js
+++
@@ -1,6 +0,0 @@
-const config = require('../knexfile')
-const knex = require('knex')(config)
-
-knex.migrate.latest([config])
-
-module.exports = knex
