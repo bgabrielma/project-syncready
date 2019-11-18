6a311fa4b09ff4d00584ff9e9342d2ea59464e0a
Bruno Martins <b.gabriel.ma@gmail.com>
2019-11-18 02:35:12 +0000

    Update css

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -17,11 +17,13 @@ const register = function(req, res) {
   let errors = {}
 
   const emptyField = { message: 'Este campo não pode estar vazio!' }
-  const differentPasswords = { message: 'As palavras-passes indicadas não correspondem uma com a outra!' }
 
-  const { fullname, email, contacto, cc, username, password, repeatpassword } = req.body
+  console.log(req.body)
 
-  if (!fullname) errors.fullname = emptyField 
+  const { fullname, address, email, contacto, cc, username, password, repeatpassword } = req.body
+
+  if (!fullname) errors.fullname = emptyField
+  if (!address) errors.address = emptyField 
   if (!email) errors.email = emptyField
   if (!cc || validadorCC(cc) !== 1) errors.cc = { message: 'O número de cartão de cidadão é inválido!' }
   if (!contacto || contacto.length !== 9) errors.contacto = { message: 'O número de telefone é inválido!' }
@@ -29,13 +31,14 @@ const register = function(req, res) {
   if (username) {
 
     // TODO: fazer sistema para verificar se tal username já existe
+    
 
   } else errors.username = emptyField
 
   if (!password) errors.password = emptyField
   if (!repeatpassword) errors.repeatpassword = emptyField
   
-  if (password != repeatpassword) errors.repeatpassword = differentPasswords
+  if (password != repeatpassword) errors.repeatpassword = { message: 'As palavras-passes indicadas não correspondem uma com a outra!' }
 
   if (Object.keys(errors).length > 0) {
     res.render('index', { title: 'SyncReady', page: 'main/register', errors , predata: req.body } )
