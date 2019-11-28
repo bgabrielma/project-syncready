e3f8b0d73b0236ffc2eb8482209cd41525ea7833
Bruno Martins <b.gabriel.ma@gmail.com>
2019-11-28 00:57:52 +0000

    Fix login page bug

--- backend/controllers/indexController.js
+++ backend/controllers/indexController.js
@@ -13,7 +13,7 @@ const main = function(req, res) {
   if (method != 'r') page = 'login'
   else page = 'register'
 
-  res.render('index', { title: 'SyncReady', page: `main/${page}`, errors: {}, predata: {} } )
+  res.render('index', { title: 'SyncReady', page: `main/${page}`, errors: {}, predata: {}, messageErrorOnInsert: null } )
 }
 
 const checkdb = async function(req, res) {
