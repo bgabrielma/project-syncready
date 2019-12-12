1aec63121a62a51048b239dfee15cd6a3b28b081
Guilherme Rodrigues <gui_srodrigues@outlook.com>
2019-12-12 15:58:57 +0000

    Finish login screen

--- app/app/src/main/java/com/example/syncreadyapp/MainActivity.java
+++ app/app/src/main/java/com/example/syncreadyapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_screen);
+        setContentView(R.layout.login);
     }
 }
