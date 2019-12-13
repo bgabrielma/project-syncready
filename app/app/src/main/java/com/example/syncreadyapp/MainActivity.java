5e7ba9fb0cccceab9186d7a0dbc66941b4ec3ed2
Guilherme Rodrigues <gui_srodrigues@outlook.com>
2019-12-13 14:06:45 +0000

    Finish register screen

--- app/app/src/main/java/com/example/syncreadyapp/MainActivity.java
+++ app/app/src/main/java/com/example/syncreadyapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register);
+        setContentView(R.layout.login);
     }
 }
