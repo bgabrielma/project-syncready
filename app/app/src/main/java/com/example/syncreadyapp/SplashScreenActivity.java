ea525c1b4a01f20c3632feb97082e64b9a374a83
Guilherme Rodrigues <gui_srodrigues@outlook.com>
2019-12-14 21:00:50 +0000

    implement splashscreen method

---
+++ app/app/src/main/java/com/example/syncreadyapp/SplashScreenActivity.java
@@ -0,0 +1,18 @@
+package com.example.syncreadyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
