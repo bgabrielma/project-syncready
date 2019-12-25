f4f62bd95acd1601943414d8cc8522491f99bb3c
Bruno Martins <b.gabriel.ma@gmail.com>
2019-12-25 16:34:16 +0000

    Update files

--- app/app/src/main/java/com/example/syncreadyapp/models/ResponseValidateRegister.java
+++
@@ -1,4 +0,0 @@
-package com.example.syncreadyapp.models;
-
-public class ResponseValidateRegister {
-}
---
+++ app/app/src/main/java/com/example/syncreadyapp/userregistervalidate/ResponseValidateRegister.java
@@ -0,0 +1,40 @@
+package com.example.syncreadyapp.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class ResponseValidateRegister {
+    @SerializedName("isCCValid")
+    @Expose
+    private Boolean isCCValid;
+    @SerializedName("isEmailValid")
+    @Expose
+    private Boolean isEmailValid;
+    @SerializedName("isUsernameValid")
+    @Expose
+    private Boolean isUsernameValid;
+
+    public Boolean getIsCCValid() {
+        return isCCValid;
+    }
+
+    public void setIsCCValid(Boolean isCCValid) {
+        this.isCCValid = isCCValid;
+    }
+
+    public Boolean getIsEmailValid() {
+        return isEmailValid;
+    }
+
+    public void setIsEmailValid(Boolean isEmailValid) {
+        this.isEmailValid = isEmailValid;
+    }
+
+    public Boolean getIsUsernameValid() {
+        return isUsernameValid;
+    }
+
+    public void setIsUsernameValid(Boolean isUsernameValid) {
+        this.isUsernameValid = isUsernameValid;
+    }
+}
