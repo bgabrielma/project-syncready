4a09c818925ad75b3f74656835fd8dea22b8f931
Bruno Martins <b.gabriel.ma@gmail.com>
2020-01-04 15:30:25 +0000

    Fixed duplicate users bug - query refactor

--- backend/controllers/usersRoomsController.js
+++ backend/controllers/usersRoomsController.js
@@ -11,6 +11,7 @@ const get = async function(req, res) {
     .innerJoin('Type_Of_User', 'Users.Type_Of_User_uuid_type_of_users', '=', 'Type_Of_User.uuid_type_of_users')
     .innerJoin('Users_has_Rooms', 'Users.pk_uuid', '=', 'Users_has_Rooms.Users_pk_uuid')
     .innerJoin('Rooms', 'Users_has_Rooms.Rooms_uuid_room', '=', 'Rooms.uuid_room')
+    .where('Rooms.uuid_room', req.query.roomUUID)
     .then(data => res.status(200).send({ ok: true, data }))
     .catch(err => res.status(500).send(err))
     
