37f2aa0cda1e5faa5862edb6c42c8acbf785504b
Bruno Martins <b.gabriel.ma@gmail.com>
2020-01-04 14:25:29 +0000

    Update room and userRooms controller. Set /user/room to return all members of that group

--- backend/controllers/usersRoomsController.js
+++ backend/controllers/usersRoomsController.js
@@ -1,19 +1,19 @@
 const db = require('../config/db')
 
 const get = async function(req, res) {
-  const instance = db('Users_has_Rooms')
 
-  const { roomUUID, userUUID } = req.query
+  if(!req.query.roomUUID) {
+    return res.status(401).send({ err: 'Users not found in room provider' })
+  }
 
-  if (roomUUID && !userUUID)
-    instance.where('Users_has_Rooms.Rooms_uuid_room', '=', `${roomUUID}`)
-  else if (!roomUUID && userUUID)
-    instance.where('Users_has_Rooms.Users_pk_uuid', '=', `${userUUID}`)
-  else instance.where('Users_has_Rooms.Rooms_uuid_room', '=', `${roomUUID}`).andWhere('Users_has_Rooms.Users_pk_uuid', '=', `${userUUID}`)
-  
-  instance
-    .then(data => res.status(200).send(data))
+  await db('Users')
+    .select('Users.*', 'Type_Of_User.type')
+    .innerJoin('Type_Of_User', 'Users.Type_Of_User_uuid_type_of_users', '=', 'Type_Of_User.uuid_type_of_users')
+    .innerJoin('Users_has_Rooms', 'Users.pk_uuid', '=', 'Users_has_Rooms.Users_pk_uuid')
+    .innerJoin('Rooms', 'Users_has_Rooms.Rooms_uuid_room', '=', 'Rooms.uuid_room')
+    .then(data => res.status(200).send({ ok: true, data }))
     .catch(err => res.status(500).send(err))
+    
 }
 
 module.exports = {
