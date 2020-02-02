41280e7eb6cf4877b0ca0155c2c2fad31b48bb09
Guilherme Rodrigues <gui_srodrigues@outlook.com>
2020-02-02 21:06:40 +0000

    modify register button and update ediit profile layout

---
+++ app/app/src/main/java/com/example/syncreadyapp/views/EditProfileActivity.java
@@ -0,0 +1,20 @@
+package com.example.syncreadyapp.views;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.example.syncreadyapp.R;
+import com.example.syncreadyapp.databinding.EditProfileBinding;
+
+public class EditProfileActivity extends AppCompatActivity {
+    private EditProfileBinding editProfileBinding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        editProfileBinding = DataBindingUtil.setContentView(this,R.layout.edit_profile);
+    }
+}
